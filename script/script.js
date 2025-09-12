48c5ce88ff2092a128effe5efe1abf3aedf67569
kingdedymulyono <githubdedyas@gmail.com>
2025-09-12 10:43:19 +0700

    first fix

--- script/script.js
+++ script/script.js
@@ -13,8 +13,10 @@ let isWork = true
 
 playButton.addEventListener("click", () => {
     if (isWork) {
+        time=0
         startWork()
     } else {
+        time=0
         startRest()
     }
 })
@@ -35,18 +37,14 @@ const restClear = () => {
 }
 
 const startWork = () => {
-    if (timeInput.value >= 60) {
-        alert("Tidak boleh lebih dari 60 menit")
-    } else {
-        if (!isPlay) { // Jika timer dalam keadaan berhenti, maka kita akan memulainya
-            // Ubah tampilan tombol menjadi pause
-            startMinute = 25
-            // startMinute = 1/6
-            time = startMinute * 60
+    if (!isPlay) { // Jika timer dalam keadaan berhenti, maka kita akan memulainya
+        // Ubah tampilan tombol menjadi pause
+        if (time > 1) {
             playButton.classList.remove("bi-play-fill");
             playButton.classList.add("bi-pause-fill");
-            timeDiv.innerHTML = '<i class="bi bi-stopwatch"></i>'
+            // timeDiv.innerHTML = '<i class="bi bi-stopwatch"></i>'
             // Mulai interval dan simpan ID-nya ke variabel global
+            console.log("interval sdg berjalan 1")
             timer = setInterval(() => {
                 let minute = Math.floor(time / 60);
                 let seconds = time % 60;
@@ -71,31 +69,17 @@ const startWork = () => {
                 }
             }, 1000);
 
-            isPlay = true; // Set status menjadi "berjalan"
+            isPlay = true; // Set status menjadi "berjalan"    
+        } else {
 
-        } else { // Jika timer sedang berjalan, maka kita akan menghentikannya
-            // Ubah tampilan tombol menjadi play
-            playButton.classList.remove("bi-pause-fill");
-            playButton.classList.add("bi-play-fill");
-
-
-            // Hentikan interval
-            clearInterval(timer);
-
-            isPlay = false; // Set status menjadi "berhenti"
-        }
-    }
-}
-const startRest = () => {
-    {
-        if (!isPlay) { // Jika timer dalam keadaan berhenti, maka kita akan memulainya
-            // Ubah tampilan tombol menjadi pause
-            startMinute = 1/6
+            startMinute = 25
+            // startMinute = 1/6
             time = startMinute * 60
             playButton.classList.remove("bi-play-fill");
             playButton.classList.add("bi-pause-fill");
             timeDiv.innerHTML = '<i class="bi bi-stopwatch"></i>'
             // Mulai interval dan simpan ID-nya ke variabel global
+            console.log("interval sdg berjalan 2")
             timer = setInterval(() => {
                 let minute = Math.floor(time / 60);
                 let seconds = time % 60;
@@ -106,7 +90,7 @@ const startRest = () => {
 
                 timeDiv.innerHTML = `${minute} : ${seconds}`;
                 time--;
-                
+
                 // Opsional: Hentikan timer saat hitungan selesai (jika waktu mencapai 0)
                 if (time < 0) {
                     clearInterval(timer);
@@ -115,7 +99,51 @@ const startRest = () => {
                     playButton.classList.remove("bi-pause-fill");
                     playButton.classList.add("bi-play-fill");
                     timeDiv.innerHTML = "00 : 00";
-                    isWork = true
+                    workClear()
+                }
+            }, 1000);
+
+            isPlay = true; // Set status menjadi "berjalan"
+        }
+    } else { // Jika timer sedang berjalan, maka kita akan menghentikannya
+        // Ubah tampilan tombol menjadi play
+        playButton.classList.remove("bi-pause-fill");
+        playButton.classList.add("bi-play-fill");
+        // Hentikan interval
+        clearInterval(timer);
+        isPlay = false; // Set status menjadi "berhenti"
+    }
+}
+const startRest = () => {
+    {
+        if (!isPlay) { // Jika timer dalam keadaan berhenti, maka kita akan memulainya
+            // Ubah tampilan tombol menjadi pause
+            startMinute = 5
+            time = startMinute * 60
+            playButton.classList.remove("bi-play-fill");
+            playButton.classList.add("bi-pause-fill");
+            // timeDiv.innerHTML = '<i class="bi bi-stopwatch"></i>'
+            // Mulai interval dan simpan ID-nya ke variabel global
+            console.log("interval sdg berjalan")
+            timer = setInterval(() => {
+                let minute = Math.floor(time / 60);
+                let seconds = time % 60;
+
+                // Tambahkan "0" di depan jika angka kurang dari 10
+                minute = minute.toString().padStart(2, '0');
+                seconds = seconds.toString().padStart(2, '0');
+
+                timeDiv.innerHTML = `${minute} : ${seconds}`;
+                time--;
+
+                // Opsional: Hentikan timer saat hitungan selesai (jika waktu mencapai 0)
+                if (time < 0) {
+                    clearInterval(timer);
+                    isPlay = false;
+                    // Atur ulang tombol dan tampilan
+                    playButton.classList.remove("bi-pause-fill");
+                    playButton.classList.add("bi-play-fill");
+                    timeDiv.innerHTML = "00 : 00";
                     restClear()
                 }
             }, 1000);
@@ -134,6 +162,8 @@ const startRest = () => {
 
 const repeat = () => {
     isPlay = false
+    time=0
+    clearInterval(timer)
     if (isWork) {
         startWork()
     } else {
@@ -146,13 +176,13 @@ buttons.forEach((btn) => {
         buttons.forEach((b) => { b.classList.remove("btn-primary") })
         playButton.classList.remove("bi-pause-fill");
         playButton.classList.add("bi-play-fill");
-        console.info(e.target.className)
         if (e.target.className.includes("rest")) {
+            console.info(e.target.className)
             document.querySelector(".work").classList.remove("text-white")
             document.querySelector(".work").classList.add("text-black")
             e.target.classList.add("text-white")
             modeBox.style.right = '-50%'
-            clearInterval(timer);
+            clearInterval(timer)
             timeDiv.innerHTML = '00:00'
             isWork = false
         } else if (e.target.className.includes("work")) {
